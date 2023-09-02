4beec3b7909af20f85eb198432f090104cc5d359
sebastian-estra <142746700+sebastian-estra@users.noreply.github.com>
2023-09-01 20:22:55 -0500

    Add files via upload

---
+++ calculadora.java
@@ -0,0 +1,12 @@
+public class calculadora {
+    public static void main(String[] args) { 
+        for (int r=1;r<=10;r++){
+            for (int c=1;c<=10;c++){
+                System.out.printf("%5d",r*c);
+            }
+                System.out.println(" ");
+        }
+        
+    }
+}
+    
